409fa73f0d95b02891cac0331d7fd874c83dc67b
Draian123 <beiteldennis3@yahoo.de>
2023-05-24 15:30:55 +0200

    SSR, SSG, ISR

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -4,7 +4,8 @@ import getUserPosts from '@/lib/getUserPosts'
 import { Suspense } from 'react'
 import UserPosts from './components/UserPost'
 import type { Metadata } from 'next'
-
+import getAllUsers from '@/lib/getAllUsers'
+import { notFound } from 'next/navigation'
 
 type Params = {
     params:{
@@ -15,6 +16,13 @@ type Params = {
 export async function generateMetadata({params: {userId}}: Params): Promise<Metadata>{
   const userData: Promise<User> = getUser(userId)
   const user: User = await userData
+  console.log(user)
+
+  if(!user) {
+    return {
+      title: 'User not found'
+    }
+  }
 
   return {
     title: user.name,
@@ -30,6 +38,8 @@ export default async function UserPage({params: {userId}}: Params) {
 
   const user= await userData
 
+  if(!user) return notFound()
+
   return (
     <>
       <h2>{user.name}</h2>
@@ -40,4 +50,13 @@ export default async function UserPage({params: {userId}}: Params) {
       </Suspense>
     </>
   )
+}
+
+export async function generateStaticParams(){
+  const usersData: Promise<User[]> = getAllUsers()  
+  const users = await usersData
+
+  return users.map(user =>(
+    {userId: user.id.toString()}
+  ))
 }
--- types.d.ts
+++ types.d.ts
@@ -6,25 +6,25 @@ type Post = {
 }
 
 type User = {
-id: number,
-name: string,
-username: string,
-email: string,
-address: {
-street: string,
-suite: string,
-city: string,
-zipcode: string,
-geo: {
-lat: string,
-lng: string
+"id": number,
+"name": string,
+"username": string,
+"email": string,
+"address": {
+"street": string,
+"suite": string,
+"city": string,
+"zipcode": string,
+"geo": {
+"lat": string,
+"lng": string
 }
 },
-phone: string,
-website: string,
-company: {
-name: string,
-catchPhrase: string,
-bs: string
+"phone": string,
+"website": string,
+"company": {
+"name": string,
+"catchPhrase": string,
+"bs": string
 }
 }
